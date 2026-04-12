3d3e822d4fee798f914aa870dee15a64a2d284b0
Jesús Cuautle <j.cuautle016@gmail.com>
2026-04-12 10:59:07 -0600

    feat: Agrega interfaces con ataques por tipo de pokemon

---
+++ src/ejercicios/pokemon/interfaces/IPlanta.java
@@ -0,0 +1,8 @@
+package ejercicios.pokemon.interfaces;
+
+public interface IPlanta {
+    public void atacarParalizar();
+    public void atacarDrenaje();
+    public void atacarHojaAfilada();
+    public void atacarLatigoCepa();
+}
